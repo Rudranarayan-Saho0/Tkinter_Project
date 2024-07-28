28d04cab57fbd54a21c1a099e0baf277d90d0aef
Rudra <rudraomm2005@gmail.com>
2024-07-28 19:58:42 +0530

    Add Tkinter Project File

---
+++ Project1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+root = Tk()
+root.geometry("500x400")
+root.title("Projrct1")
+title_label = Label(text = "Welcome To Project Ready")
+title_label.pack()
+
+
+
+root.mainloop()
